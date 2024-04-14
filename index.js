97da49812fb6bc756870f9dd8b71bd231b4c27fb
Michael <mikebook24@gmail.com>
2024-04-13 23:55:28 -0500

    improve readability

--- index.js
+++ index.js
@@ -4,40 +4,45 @@ const quadrants = [
     document.querySelector('#right-foot'),
     document.querySelector('#left-foot')
 ]
+
 const colors = ['red', 'blue', 'yellow', 'green']
 
 let counter = 0
 let index = 0
 let spinning = false
 
-const nextQuadrant = () => {
-    index = counter++ % quadrants.length
-    return quadrants[index]
-}
-const clearSelected = () => quadrants[index].classList.remove('active', 'red', 'blue', 'green', 'yellow')
-const randomColor = () => colors[Math.floor(Math.random() * colors.length)]
-
 function spin() {
     if (!spinning) {
         spinning = true
-        clearSelected()
-
-        const interval = setInterval(() => {
+        clearSelectedQuadrant()
+        const spinInterval = setInterval(() => {
             const quadrant = nextQuadrant()
             quadrant.classList.add('white')
             setTimeout(() => quadrant.classList.remove('white'), 100)
         }, 100)
-
         setTimeout(() => {
-            clearInterval(interval)
+            clearInterval(spinInterval)
             setTimeout(() => {
                 const quadrant = nextQuadrant()
                 quadrant.classList.add('active', randomColor())
                 spinning = false
             }, 100)
-        }, Math.floor(Math.random() * (2500 - 1000 + 1)) + 1000)
+        }, spinDuration())
     }
 }
+
+const clearSelectedQuadrant = () => {
+    quadrants[index].classList.remove('active', 'red', 'blue', 'green', 'yellow')
+}
+
+const nextQuadrant = () => {
+    index = counter++ % quadrants.length
+    return quadrants[index]
+}
+
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)]
+
+const spinDuration = () => Math.floor(Math.random() * (2500 - 1000 + 1)) + 1000
 
 document.body.addEventListener('click', spin)
 document.body.onkeyup = (e) => {
